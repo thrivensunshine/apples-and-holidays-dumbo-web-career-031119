f760dd5c499174ff921eb44ef7bb9fae1cd7e2f4
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-24 23:38:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,10 +26,8 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
 
-holiday_hash[season] = holiday_name 
-holiday_hash[season][holiday_name] = supply_array
-
-
+ holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
